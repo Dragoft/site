340bd67fda4dac246c62789dc13500e2ed19a805
白龙巜丶 <124327251+Haku-Dragon-s-home@users.noreply.github.com>
2023-02-03 11:20:10 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,47 @@
+﻿window.onload = () => {
+    const menu = document.querySelector('.menu')
+    const menuHeight = menu.offsetHeight - parseInt(getComputedStyle(menu)['paddingTop']) - parseInt(getComputedStyle(menu)['paddingBottom'])
+    menu.style.height = '0'
+
+    openMenu = e => {
+        e.preventDefault()
+
+        // 防止菜单超出界面
+        var h = document.body.clientHeight - 180;
+        var w = document.body.clientWidth - 170;
+
+        if(e.clientX>w){
+                menu.style.left = `${w}px`
+        } else {
+                menu.style.left = `${e.clientX}px`
+        }
+
+        if(e.clientY>h){
+                menu.style.top = `${e.pageY - 175}px`
+        } else {
+                menu.style.top = `${e.pageY + 5}px`
+        }
+
+        menu.classList.add('is-active')
+        menu.style.height = `${menuHeight}px`
+
+        return false
+    }
+
+    closeMenu = () => {
+        menu.style.height = '0'
+        menu.classList.remove('is-active')
+    }
+    window.onclick = () => closeMenu()
+}
+
+
+
+//Other functions
+
+ function fun_0(){
+	document.execCommand("Copy");	
+};
+ function fun_1(){
+	location.reload();
+};
