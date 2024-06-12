1087fabfcfe98a2cbbec40ecf695b3f2216c1d88
Dragoft <16841977@qq.com>
2024-06-12 22:15:30 +0800

    Add files via upload

--- blog/pageData.js
+++ blog/pageData.js
@@ -2,23 +2,23 @@
 
 	{
 		name: '关于网站',
-		src: 'Page/about/',
+		src: 'page/about/',
 		details: '#2023.6.7\n网站信息 & 大事记 & 特别感谢',
 		type: 'hide',
 	},
 	{
 		name: '广场',
-		src: 'Page/square/',
+		src: 'page/square/',
 		details: '#2024.6.8\n网站广场',
 		type: 'hide',
 	},
+
 	{
 		name: '画室',
 		src: '20231002/',
 		details: '#2023.10.2\n平时画的一些动漫人物铅笔稿',
 		type: '',
 	},
-
 	{
 		name: '神隠し',
 		src: '20240610/',
@@ -32,7 +32,6 @@
 
 
 /* 
-	懒人专属复制区
 
 	{
 		name: '',
@@ -42,6 +41,6 @@
 	},
 
 	TYPE:
-	图册:	文章:	实验:	教程: 
+	图集:	文章:	实验:	教程: 
 
  */
--- functions/remark.api.js
+++ functions/remark.api.js
@@ -5,7 +5,7 @@
 	var reData = {"Mode": mode, "Content": cont};
 
 
-
+	// 删除留言
 	if (mode=="0") {
 		if (cont!=null) {
 			var ps = context.env.MetaDB.prepare('DELETE FROM comment WHERE deletable = 1 and id = "' + cont + '"');
@@ -18,11 +18,12 @@
 		}
 	}
 
+	// 添加留言
 	if (mode=="1") {
 		if (cont!=null) {
 			var cont = cont.split("###");
 
-			// id
+			// 生成一个 id
 			var now = new Date();
 			var options = {timeZone: 'Asia/Shanghai', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
 			var formatter = new Intl.DateTimeFormat('en-US', options);
@@ -41,6 +42,7 @@
 		}
 	}
 
+	// 读取留言数据
 	if (mode=="2") {
 		if (cont!=null) {
 			var ps = context.env.MetaDB.prepare('SELECT * FROM comment ORDER BY id DESC LIMIT ' + cont + ', 10');
@@ -48,6 +50,7 @@
 		}
 	}
 
+	// 获取留言总数
 	if (mode=="3") {
 		var ps = context.env.MetaDB.prepare('SELECT * from root where data="comment"');
 		reData = await ps.first();
