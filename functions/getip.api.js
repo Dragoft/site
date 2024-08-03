e6feb607484a3c73db98a722e128b2492f843992
Dragoft <16841977@qq.com>
2024-08-03 21:32:20 +0800

    UPDATE

--- functions/getip.api.js
+++
@@ -1,6 +0,0 @@
-﻿export async function onRequest(context) {
-	var reData = {"API": mode};
-	var reData = context.request.headers.get('X-Forwarded-For') || context.request.headers.get('CF-Connecting-IP');
-
-	return Response.json(reData);
-}
