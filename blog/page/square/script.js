daccf45a81347b1a4509a5d5c03c609d2f78a6cc
Dragoft <16841977@qq.com>
2024-10-20 00:06:33 +0800

    UPDATE

--- blog/page/square/script.js
+++ blog/page/square/script.js
@@ -1,272 +1,180 @@
 ﻿/*
 	This script is used to interact with a database...
-	Last change: 2024/8/27
+	Last change: 2024/10/19
 */
 
-function sent() {
-	if (wait==0) {
-		var name = document.querySelector('.Tname').value.replace(/\n/g, '');
-		var content = document.querySelector('.Tcontent').value;
 
-		if (name.length <= 0 || name.length > 20) {
-			return	
-		}
 
-		if (content.length <= 0 || content.length > 200) {
-			return
-		}
+const env = {data: {}, f: {}, tmp: {}, timer: {}}
 
-		wait = 1;
-		message('<span class="t-8" ></span> 加载中', 'info', -1);
+env.data.db = null
+env.data.page = 1
+env.data.pause = false
+env.data.remarkN = null
+env.data.pageN = null
 
-		fetch('https://tatsuno.top/remark.api', {
-			method: "POST",
-			headers: {
-				"Token": 1
-			},
-			body: JSON.stringify({
-				"name": name,
-				"content": content,
-			})
-		})
-		.then(response => {
-			if (!response.ok) {
-				if (response.status === 429) {
-					fetchfailed();
-				}
-			} else {
-				return response.json();
-			}
-		}); 
 
-		if (errN==0) {
-			document.querySelector('.Tcontent').value = '';
-			document.querySelector('.Tcontent').style.height = '48px';
-			document.querySelector('.count').innerHTML = '剩余 200 字';
+// 发送评论
+env.f.sent = function() {
+	if (env.data.pause) {return}
 
-			ComNum = Number(ComNum) + 1;
-			PageAll = Math.ceil(ComNum/10);
-			document.querySelector('.page0').innerHTML = PageAll;
-			document.querySelector('.page').title = '共 ' + ComNum + ' 条留言';
+	var name = document.querySelector('.Tname').value.replace(/\n/g, '');
+	var content = document.querySelector('.Tcontent').value;
 
-			setTimeout(function (){
-				loadComment(1);
-				page = 1;
-			}, 1500);
-		}
+	if (name.length <= 0 || name.length > 20 || content.length <= 0 || content.length > 200) {
+		return	
 	}
-}
 
 
 
-function loadComment(page) {
+	env.data.pause = true
+
 	fetch('https://tatsuno.top/remark.api', {
 		method: "POST",
 		headers: {
-			"Token": 2,
+			"Token": 1
 		},
 		body: JSON.stringify({
-			"page": (page - 1) * 10,
+			"name": name,
+			"content": content,
 		})
 	})
 	.then(response => {
 		if (!response.ok) {
-			if (response.status === 429) {
-				fetchfailed();
+				env.f.failed()
 			}
-		} else {
-			return response.json();
-		}
-	})
-	.then(json => showComment(json)); 
-}
+	}); 
 
+		document.querySelector('.Tcontent').value = '';
+		document.querySelector('.Tcontent').style.height = '48px';
+		document.querySelector('.count').innerHTML = '剩余 200 字';
 
-function showComment(data) {
-	if (errN==0) {
-		var box = document.querySelector('.commentBox');
-		box.style.opacity = '0';
-		box.innerHTML = '';
+		env.data.remarkN ++
+		env.data.pageN = Math.ceil(env.data.remarkN / 10);
+		document.querySelector('.page0').innerHTML = env.data.pageN;
+		document.querySelector('.page').title = '共 ' +env.data.remarkN + ' 条留言';
 
 		setTimeout(function (){
-			// 获取当前时间
-			var now = new Date();
-			var options = {timeZone: 'Asia/Shanghai', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
-			var formatter = new Intl.DateTimeFormat('en-US', options);
-			var parts = formatter.formatToParts(now).reduce((acc, part) => ({ ...acc, [part.type]: part.value }), {});
-			var time = `${parts.year}-${parts.month}-${parts.day} ${parts.hour}:${parts.minute}:${parts.second}`.replace(/:/g, '').replace(/-/g, '').replace(/ /g, '');
-
-			for (var i = 0; i < data['results'].length; i++) {
-				var span = document.createElement('span');
-				span.setAttribute('class', 'comment');
-				box.appendChild(span);
-
-				// 昵称
-				var span1 = document.createElement('span');
-				span1.innerText = data.results[i].name.replace(/\n/g, '') + ':  ';
-				span1.setAttribute('class', 'title');
-				span.appendChild(span1);
-
-				// 内容
-				var span2 = document.createElement('span');
-				span2.innerText = data.results[i].content;
-				span2.setAttribute('class', 'content');
-				span.appendChild(span2);
-
-				if (span2.offsetHeight>120) {
-					span2.setAttribute('style', 'height: 120px');
-
-					var span4 = document.createElement('span');
-					span4.innerHTML = '[ 展开 ]';
-					span4.setAttribute('class', 'open');
-					span4.setAttribute('onclick', '$(this).css("display", "none");$(this).siblings(".content").css("height", "unset");');
-					span.appendChild(span4);
-				}
-
-				// 日期
-				var span3 = document.createElement('span');
-				span3.innerText = '[' + data.results[i].id.substring(0, 16) + ']';
-				span3.setAttribute('class', 'time');
-				span.appendChild(span3);
-
-				if (data['results'][i]['deletable']=="1") {
-
-					// 超过 7 天的留言无法删除
-					if (parseInt(data.results[i].id.replace(/:/g, '').replace(/-/g, '').replace(/ /g, '')) + 7000000 > parseInt(time)) {
-						var a = document.createElement('a');
-						a.innerHTML = '删除'
-						a.setAttribute('class', 'delete');
-						a.setAttribute('onclick', 'deleteComment("' + data['results'][i]['id'] + '")');
-						span3.appendChild(a);
-					}
-				}
-
-				if (data['results'][i]['deletable']=="0") {
-					var span5 = document.createElement('span');
-					span5.innerHTML = 'admin'
-					span5.setAttribute('class', 'admin');
-					span3.appendChild(span5);
-				}
-			}
-		}, 500);
-
-		setTimeout(function (){
-			message.Close()
-			wait = 0;
-			box.style.opacity = '1';
-		}, 1000);
-	}
+			env.f.print(1)
+			env.data.page = 1
+		}, 1500);
 }
 
-function deleteComment(id) {
-	$('#body').addClass('body-scroll');
-	$('.del').fadeIn(150);
-	document.querySelector('.del-id').innerHTML = id;
-	document.querySelector('.delbtn').setAttribute('onclick', 'Delete(`' + id + '`)');
-}
 
-function deleteCancel() {
-	$('.del').fadeOut(200);	
-	$('#body').removeClass('body-scroll');
-}
-
-function Delete(id) {
-	if (wait==0) {
-		wait = 1;
-		$('.del').fadeOut(200);	
-		$('#body').removeClass('body-scroll');
-
-		setTimeout(function (){
-			message('<span class="t-8" ></span> 加载中', 'info', -1);
-			$('#body').removeClass('body-scroll');
-		}, 1000);
-
+// 显示评论
+env.f.print = function(page) {
+	if (env.data.ok) {return}
+	if (page != null) {
 		fetch('https://tatsuno.top/remark.api', {
 			method: "POST",
 			headers: {
-				"Token": 0,
+				"Token": 2,
 			},
 			body: JSON.stringify({
-				"id": id,
+				"page": (page - 1) * 10,
 			})
 		})
 		.then(response => {
 			if (!response.ok) {
-				if (response.status === 429) {
-					fetchfailed();
-				}
+				env.f.fail();
 			} else {
 				return response.json();
 			}
+		})
+		.then(json => {
+			env.data.db = json
 		}); 
-
-		if (errN==0) {
-			ComNum = Number(ComNum) - 1;
-			PageAll = Math.ceil(ComNum/10);
-			document.querySelector('.page0').innerHTML = PageAll;
-			document.querySelector('.page').title = '共 ' + ComNum + ' 条留言';
-
-			setTimeout(function (){
-				loadComment(page);
-			}, 2000);
-		}
 	}
-}
 
-function init() {
-	page = 1
+	// 打印评论
 
-	if (wait==0) {
-		wait = 1;
-		message('<span class="t-8" ></span> 加载中', 'info', -1);
-		loadComment(page);
+	var box = document.querySelector('.commentBox');
+	box.style.opacity = '0';
+	box.innerHTML = '';
 
-		fetch('https://tatsuno.top/remark.api', {
-			method: "POST",
-			headers: {
-				"Token": 3,
-			},
-			body: JSON.stringify({})
-		})
-		.then(response => {
-			if (!response.ok) {
-				if (response.status === 429) {
-					fetchfailed();
+	setTimeout(function (){
+		// 获取当前时间
+		env.f.getTime()
+
+		for (var i = 0; i < env.data.db['results'].length; i++) {
+			var span = document.createElement('span');
+			span.setAttribute('class', 'comment');
+			box.appendChild(span);
+
+			// 昵称
+			var span1 = document.createElement('span');
+			span1.innerText = env.data.db.results[i].name.replace(/\n/g, '') + ':  ';
+			span1.setAttribute('class', 'title');
+			span.appendChild(span1);
+
+			// 内容
+			var span2 = document.createElement('span');
+			span2.innerText = env.data.db.results[i].content;
+			span2.setAttribute('class', 'content');
+			span.appendChild(span2);
+
+			if (span2.offsetHeight>120) {
+				span2.setAttribute('style', 'height: 120px');
+
+				var span4 = document.createElement('span');
+				span4.innerHTML = '[ 展开 ]';
+				span4.setAttribute('class', 'open');
+				span4.setAttribute('onclick', '$(this).css("display", "none");$(this).siblings(".content").css("height", "unset");');
+				span.appendChild(span4);
+			}
+
+			// 日期
+			var span3 = document.createElement('span');
+			span3.innerText = '[' + env.data.db.results[i].id.substring(0, 16) + ']';
+			span3.setAttribute('class', 'time');
+			span.appendChild(span3);
+
+			if (env.data.db['results'][i]['deletable']=="1") {
+
+				// 超过 7 天的留言无法删除
+				if (parseInt(env.data.db.results[i].id.replace(/:/g, '').replace(/-/g, '').replace(/ /g, '')) + 7000000 > parseInt(time)) {
+					var a = document.createElement('a');
+					a.innerHTML = '[删除]'
+					a.setAttribute('class', 'delete');
+					a.setAttribute('onclick', 'env.f.del($(this), "' + env.data.db['results'][i]['id'] + '")');
+					span3.appendChild(a);
 				}
-			} else {
-				return response.json();
 			}
-		})
-		.then(json => getComNum(json)); 
 
-		function getComNum(data) {
-			if (errN==0) {
-				ComNum = Number(data.results[0].content);
-				PageAll = Math.ceil(ComNum/10);
-				document.querySelector('.page0').innerHTML = PageAll;
-				document.querySelector('.page').title = '共 ' + ComNum + ' 条留言';
+			if (env.data.db['results'][i]['deletable'] == "0") {
+				var span5 = document.createElement('span');
+				span5.innerHTML = 'admin'
+				span5.setAttribute('class', 'admin');
+				span3.appendChild(span5);
 			}
 		}
-	}
+	}, 500);
+
+	setTimeout(function (){
+		env.data.pause = false
+		box.style.opacity = '1'
+		
+	}, 1000);
+
 }
 
-function turnPage(n) {
-	if (wait==0) {
-		if (page + n >= 1) {
-			if (page + n <= PageAll) {
-				wait = 1;
-				message('<span class="t-8" ></span> 加载中', 'info', -1);
+env.f.fail = function() {
+	env.data.pause = true
+} 
 
-				page = page + n
-				document.querySelector('.Tpage').value = page;
-				loadComment(page);
-			}
-		}
-	}
+// 授时
+env.f.getTime = function() {
+	var now = new Date();
+	var options = {timeZone: 'Asia/Shanghai', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
+	var formatter = new Intl.DateTimeFormat('en-US', options);
+	var parts = formatter.formatToParts(now).reduce((acc, part) => ({ ...acc, [part.type]: part.value }), {});
+
+	env.data.time  = `${parts.year}-${parts.month}-${parts.day} ${parts.hour}:${parts.minute}:${parts.second}`.replace(/:/g, '').replace(/-/g, '').replace(/ /g, '');
+	return env.data.time
 }
 
-// 日期格式化	blog.csdn.net/qq_42415827/article/details/114630461
-function dateFormatter(formatter, date) {
+// 日期格式化
+env.f.dateFormatter = function(formatter, date) {
 	date = (date ? new Date(date) : new Date)
 	const Y = date.getFullYear() + '',
 		M = date.getMonth() + 1,
@@ -283,61 +191,60 @@ function dateFormatter(formatter, date) {
 		.replace(/ss/g, (s < 10 ? '0' : '') + s)
 }
 
+// 初始化
+env.f.init = function() {
+	if (env.data.pause) {return}
+	env.f.print(env.data.page);
 
+	fetch('https://tatsuno.top/remark.api', {
+		method: "POST",
+		headers: {
+			"Token": 3,
+		},
+		body: JSON.stringify({})
+	})
+	.then(response => {
+		if (!response.ok) {
+				env.f.fail()
+		} else {
+			return response.json()
+		}
+	})
+	.then(json => {
+		env.data.remarkN = Number(json.results[0].content);
+		env.data.pageN = Math.ceil(ComNum/10);
+		document.querySelector('.page0').innerHTML = env.data.pageN;
+		document.querySelector('.page').title = '共 ' + env.data.remarkN + ' 条留言';
+	}); 
+}
 
-// 弹窗
-function message(content, type, time) {
-	$('.message-box').html(content);
-	$('.message').addClass('message-active');
-	if (time != -1) {
-		setTimeout(function (){
-			$('.message').removeClass('message-active');
-		}, time);
-	}
-
-	// 关闭消息框
-	message.Close=function(){
-		$('.message').removeClass('message-active');
+// 跳转指定页数
+env.f.toPage = function(page) {
+	if (env.data.pause) {return}
+	if (env.data.page + page >= 1 && env.data.page + page <= env.data.pageN) {
+		env.data.page = env.data.page + page
+		document.querySelector('.Tpage').value = env.data.page;
+		env.f.print(env.data.page)
 	}
 }
 
-function fetchfailed() {
-	errN = 1;
-	wait = 1;
-	message.Close();
-	setTimeout(function (){
-		wait = 1;
-		message('<span class="t-8" ></span>　请求速度过快，请稍候', 'info', -1);
-	}, 1000);
-	setTimeout(function (){
-		wait = 0;
-		errN = 0;
-		message.Close();
-	}, 11000);
-} 
-
-
-function escapeRegExp(string) {
-	return string.replace(/[\r\n\s]+/g, '');
-}
-
-function YautoResize(e) {
+env.f.yResize = function(e) {
 	const t = document.querySelector(e);
 	t.style.height = 'auto';
 	t.style.height = t.scrollHeight + 'px';
 	$("html,body").animate({scrollTop: $(document).height()}, 0);
 }
 
-function XautoResize(e) {
+env.f.xResize = function(e) {
 	const t = document.querySelector(e);
 	const h = document.querySelector('.Tname-2');
 
 	h.innerHTML = t.value;
-	t.value = escapeRegExp(t.value)
+	t.value = t.value.replace(/[\r\n\s]+/g, '');
 	t.style.width = (h.offsetWidth || 32) + 'px';
 }
 
-function addOpen() {
+env.f.open = function() {
 	if($('.add').hasClass('add-active')!=true) {
 		$('.add').addClass('add-active');
 		$('.new').addClass('new-active');
@@ -350,5 +257,39 @@ function addOpen() {
 		$('.bar2').fadeIn(300);
 	}
 }
+
+env.f.del = function(e, id) {
+	e.html(`<a href="javascript:;" onclick="env.f.del.yes('` + id + `')" >确认删除？</a>`)
+	e.attr('onclick', '')
+}
+	env.f.del.yes = function(id) {
+		fetch('https://tatsuno.top/remark.api', {
+			method: "POST",
+			headers: {
+				"Token": 0,
+			},
+			body: JSON.stringify({
+				"id": id,
+			})
+		})
+		.then(response => {
+			if (!response.ok) {
+				env.f.fail();
+			} else {
+				return response.json();
+			}
+		}); 
+
+		if (!env.data.pause) {
+			env.f.pageN --
+			env.f.remarkN = Math.ceil(env.f.pageN/10);
+			document.querySelector('.page0').innerHTML = env.f.pageN;
+			document.querySelector('.page').title = '共 ' + env.f.remarkN + ' 条留言';
+
+			setTimeout(function (){
+				env.f.print(env.f.page);
+			}, 2000);
+		}
+	}
 
 
